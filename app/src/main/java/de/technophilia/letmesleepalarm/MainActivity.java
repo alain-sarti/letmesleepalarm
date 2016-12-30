872386b5dc0c20d89e7951eb3ab314fe675625e5
Alain Sarti <alain.sarti@gmail.com>
2016-12-30 13:38:16 +0100

    Initial commit.

---
+++ app/src/main/java/de/technophilia/letmesleepalarm/MainActivity.java
@@ -0,0 +1,27 @@
+package de.technophilia.letmesleepalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnAddAlarm)
+    Button btnAddAlarm;
+
+    @OnClick(R.id.btnAddAlarm)
+    public void addAlarm() {
+        Toast.makeText(this, "Test-Alarm", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
