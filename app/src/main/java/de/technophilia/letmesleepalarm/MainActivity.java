2579d75c55953b25f93bee52156d7297bd537ff8
Alain Sarti <alain.sarti@gmail.com>
2016-12-31 16:57:08 +0100

    Tried out displaying a notification, which worked. Playing the Alarm Sound did work, but needs a way to stop :)

--- app/src/main/java/de/technophilia/letmesleepalarm/MainActivity.java
+++ app/src/main/java/de/technophilia/letmesleepalarm/MainActivity.java
@@ -1,5 +1,11 @@
 package de.technophilia.letmesleepalarm;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,9 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btnAddAlarm)
     Button btnAddAlarm;
 
+    private static long OFFSET = 1000 * 30; // 1000 * 60 * 15
+    private static long TIME = 1000 * 30; // 1000 * 60 * 60 * 8
+
     @OnClick(R.id.btnAddAlarm)
     public void addAlarm() {
-        Toast.makeText(this, "Test-Alarm", Toast.LENGTH_SHORT).show();
+        scheduleNotification(getNotification("WAKE UP!!!!"), OFFSET, TIME);
     }
 
     @Override
@@ -23,5 +32,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    private void scheduleNotification(Notification notification, long delay, long time) {
+        // Intent notificationIntent = new Intent(this, AlarmNotificationPublisher.class);
+        Intent notificationIntent = new Intent(this, AlarmPlaySoundPublisher.class);
+        notificationIntent.putExtra(AlarmNotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(AlarmNotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay + time;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Let Me Sleep Notification");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        return builder.build();
     }
 }
