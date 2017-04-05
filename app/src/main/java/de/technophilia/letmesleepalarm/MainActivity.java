6c114107c7ab54c550343b88f092e7ddd71539eb
Alain Sarti <alain.sarti@gmail.com>
2017-04-05 21:18:49 +0200

    more refactoring. The MainActivity is much more simple now, I like it :)

--- app/src/main/java/de/technophilia/letmesleepalarm/MainActivity.java
+++ app/src/main/java/de/technophilia/letmesleepalarm/MainActivity.java
@@ -7,9 +7,9 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.widget.EditText;
@@ -23,6 +23,7 @@ import butterknife.OnClick;
 import de.technophilia.letmesleepalarm.alarm.AlarmNotificationReceiver;
 import de.technophilia.letmesleepalarm.alarm.AlarmPlaySoundPublisher;
 import de.technophilia.letmesleepalarm.job.AlarmJob;
+import de.technophilia.letmesleepalarm.util.IntentFactory;
 import de.technophilia.letmesleepalarm.util.NotificationFactory;
 import de.technophilia.letmesleepalarm.util.Settings;
 
@@ -35,15 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnAddAlarm)
     public void addAlarm() {
-        scheduleNotification(getOffset(), getTime());
+        scheduleNotification(calculateOffset(), calculateTime());
+        scheduleAlarm(calculateOffset(), calculateTime());
     }
 
     @OnClick(R.id.btnAddJob)
     public void addJob() {
-        scheduleJob(getOffset(), getTime());
+        scheduleNotification(calculateOffset(), calculateTime());
+        scheduleJob(calculateOffset(), calculateTime());
     }
 
-    private long getTime() {
+    private long calculateTime() {
         long time = 1000 * 60 * 60 * 8;
         if(!TextUtils.isEmpty(etHours.getText().toString())) {
             time = Long.parseLong(etHours.getText().toString()) * 1000 * 60 * 60;
@@ -52,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         return time;
     }
 
-    private long getOffset() {
+    private long calculateOffset() {
         long offset = 1000 * 60 * 15;
         if(!TextUtils.isEmpty(etOffset.getText().toString())) {
             offset = Long.parseLong(etOffset.getText().toString()) * 1000 * 60;
@@ -69,35 +72,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void scheduleNotification(long delay, long time) {
-        PendingIntent notificationPendingIntent = prepareAlarmIntent(delay, time);
-        Notification notification = NotificationFactory.buildNotification(getAlarmTime(delay, time), notificationPendingIntent, this);
+        Notification notification = NotificationFactory.buildNotification(getAlarmTime(delay, time), IntentFactory.prepareNotificationIntent(this), this);
         ((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE)).notify(Settings.NOTIFICATION_ID, notification);
 
         Toast.makeText(this, this.getString(R.string.toast_alarm_set, DateFormat.format("hh:mm", getAlarmTime(delay, time))), Toast.LENGTH_LONG).show();
     }
 
-    private Date getAlarmTime(long delay, long time) {
-        return new Date(System.currentTimeMillis() + delay + time);
-    }
-
-    private PendingIntent prepareAlarmIntent(long delay, long time) {
-        Intent alarmIntent = new Intent(this, AlarmPlaySoundPublisher.class);
-        Intent notificationIntent = new Intent(this, AlarmNotificationReceiver.class);
-        PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), Settings.ALARM_ID, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        PendingIntent notificationPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        setAlarm(SystemClock.elapsedRealtime() + delay + time, alarmPendingIntent);
-
-        return notificationPendingIntent;
-    }
-
-    private void setAlarm(long futureInMillis, PendingIntent alarmPendingIntent) {
+    private void scheduleAlarm(long delay, long time) {
         AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         if(Build.VERSION.SDK_INT >= 23) {
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, alarmPendingIntent);
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + delay + time, IntentFactory.prepareAlarmIntent(this));
         } else {
-            alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, alarmPendingIntent);
+            alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + delay + time, IntentFactory.prepareAlarmIntent(this));
         }
+    }
+
+    private Date getAlarmTime(long delay, long time) {
+        return new Date(System.currentTimeMillis() + delay + time);
     }
 
     private void scheduleJob(long offset, long time) {
--- app/src/main/java/de/technophilia/letmesleepalarm/util/IntentFactory.java
+++ app/src/main/java/de/technophilia/letmesleepalarm/util/IntentFactory.java
@@ -1,8 +1,25 @@
 package de.technophilia.letmesleepalarm.util;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import de.technophilia.letmesleepalarm.alarm.AlarmNotificationReceiver;
+import de.technophilia.letmesleepalarm.alarm.AlarmPlaySoundPublisher;
+
 /**
  * Created by alainsarti on 04/04/2017.
  */
 
 public class IntentFactory {
+
+    public static PendingIntent prepareAlarmIntent(Context context) {
+        Intent alarmIntent = new Intent(context, AlarmPlaySoundPublisher.class);
+        return PendingIntent.getBroadcast(context.getApplicationContext(), Settings.ALARM_ID, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public static PendingIntent prepareNotificationIntent(Context context) {
+        Intent notificationIntent = new Intent(context, AlarmNotificationReceiver.class);
+        return PendingIntent.getBroadcast(context.getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
 }
